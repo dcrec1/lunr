a0ef0ed5fa8448f5e5ecf25bdc59c8f6f56e3eec
Diego Carrion <dc.rec1@gmail.com>
2010-02-05 15:20:43 -0200

    updated ActiveLucene

--- vendor/plugins/active_lucene/lib/active_lucene.rb
+++ vendor/plugins/active_lucene/lib/active_lucene.rb
@@ -2,11 +2,19 @@ require 'java'
 require 'rubygems'
 require 'active_support'
 
-Dir[File.expand_path(File.dirname(__FILE__) + "/*.jar")].each { |path| require path.split('/').last.gsub('.jar', '') }
+Dir[File.expand_path(File.dirname(__FILE__) + "/*.jar")].each do |path| 
+  require path.split('/').last.gsub('.jar', '')
+end
+
 import org.apache.lucene.document.Field
+
 import org.apache.lucene.store.FSDirectory
+
+import org.apache.lucene.index.IndexReader
 import org.apache.lucene.index.IndexWriter
+
 import org.apache.lucene.analysis.standard.StandardAnalyzer
+
 import org.apache.lucene.queryParser.standard.StandardQueryParser
 
 import org.apache.lucene.search.IndexSearcher
@@ -15,17 +23,28 @@ import org.apache.lucene.search.BooleanQuery
 import org.apache.lucene.search.MatchAllDocsQuery
 import org.apache.lucene.search.WildcardQuery
 
+import org.apache.lucene.search.spell.LuceneDictionary;
+import org.apache.lucene.search.spell.SpellChecker;
+
 import org.apache.lucene.search.highlight.QueryScorer
 import org.apache.lucene.search.highlight.Highlighter
 
 import org.apache.lucene.util.Version
 
-APP_ROOT ||= RAILS_ROOT if defined?(RAILS_ROOT)
-APP_ENV ||= RAILS_ENV if defined?(RAILS_ENV)
+if defined? RAILS_ROOT
+  APP_ROOT = RAILS_ROOT
+  APP_ENV  = RAILS_ENV
+elsif not defined? APP_ROOT
+  APP_ROOT = '.'
+  APP_ENV  = 'default'
+end
 
-%w(analyzer document index query searcher term writer).each { |name| require "active_lucene/#{name}" }
+%w(analyzer document index dictionary query reader search_result searcher suggest term writer).each do |name| 
+  require "active_lucene/#{name}"
+end
 
 module ActiveLucene
   ID = 'id'
   ALL = '_all'
+  TYPE = '_type'
 end
