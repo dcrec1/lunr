f99eeffc2edbd3098d680fba4abfc290a71f5891
Diego Carrion <dc.rec1@gmail.com>
2010-02-21 13:59:49 -0300

    skinny controllers, fat models

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -11,26 +11,13 @@ class DocumentsController < InheritedResources::Base
     end
   end
   
+  protected
+  
   def collection
     @documents = end_of_association_chain.paginate :page => params[:page]
   end
   
-  protected
-  
   def extract_html
-    if request.content_type.eql? Mime::HTML
-      document = Nokogiri::HTML(html_or_io)
-      document.search("script").each &:unlink
-      params[:document] = { :head => element("head", :from => document), 
-                            :body => element("body", :from => document) }
-    end
-  end
-  
-  def element(name, opts)
-    opts[:from].search(name).first.content
-  end
-  
-  def html_or_io
-    request.raw_post.lstrip.start_with?("<") ? request.raw_post : Net::HTTP.get(URI.parse(request.raw_post))
+    params[:document] = Html.parse(request.raw_post).to_hash if request.content_type.eql? Mime::HTML
   end
 end
