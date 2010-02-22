d530a1622e197cdbda294ccae64f967fcd8ce81f
Diego Carrion <dc.rec1@gmail.com>
2010-02-22 16:12:29 -0300

    changed pdf-reader for pdf-toolkit

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails::Initializer.run do |config|
   config.gem 'haml'
   config.gem 'crack'
   config.gem 'nokogiri'
-  config.gem 'pdf-reader'
+  config.gem 'pdf-toolkit'
   config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
   config.time_zone = 'UTC'
 end
